996ff0ea246b9b1ada7abe0ce0830f913eda446b
BT <bahartofigh@gmail.com>
2023-01-17 23:09:34 +0330

    contact section.

---
+++ Js/Index.js
@@ -0,0 +1,6 @@
+const navbarScroll = () => {
+    const navbar = document.querySelector(".nav-container");
+    this.scrollY >= 50 ?  navbar.classList.add('navbarScrolled')  : navbar.classList.remove('navbarScrolled');
+}
+
+window.addEventListener('scroll', navbarScroll)
