0e4c212e39e4beb3dc1a59ba928407b2cbbe8261
BT <bahartofigh@gmail.com>
2023-02-06 14:17:34 +0330

    add navbar scroll styles.

--- Js/Index.js
+++ Js/Index.js
@@ -1,6 +1,7 @@
+// ===  Header Styles On Scroll  ===
 const navbarScroll = () => {
     const navbar = document.querySelector(".nav-container");
-    this.scrollY >= 50 ?  navbar.classList.add('navbarScrolled')  : navbar.classList.remove('navbarScrolled');
+    this.scrollY >= 50 ?  navbar.classList.add('navbarScrolled') : navbar.classList.remove('navbarScrolled');
 }
 
 window.addEventListener('scroll', navbarScroll)
