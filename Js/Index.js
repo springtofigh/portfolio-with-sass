0642d8b32d442a044a7d8bbf856f8c1f0b0a0d6f
BT <bahartofigh@gmail.com>
2023-02-06 17:40:48 +0330

    add text effect animation in home section.

--- Js/Index.js
+++ Js/Index.js
@@ -4,4 +4,21 @@ const navbarScroll = () => {
     this.scrollY >= 50 ?  navbar.classList.add('navbarScrolled') : navbar.classList.remove('navbarScrolled');
 }
 
-window.addEventListener('scroll', navbarScroll)
+window.addEventListener('scroll', navbarScroll);
+
+// const swiper = new Swiper('.swiper', {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 2400,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
