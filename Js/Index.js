7b1c63653217b5294ced1c1ba1458bfba12b37b0
BT <bahartofigh@gmail.com>
2023-03-07 00:42:59 +0330

    add project Section tabs active style.

--- Js/Index.js
+++ Js/Index.js
@@ -6,6 +6,7 @@ const navbarScroll = () => {
 
 window.addEventListener('scroll', navbarScroll);
 
+// ===  Slider For Skills Section  ===
 const swiper = new Swiper(".swiper", {
     slidesPerView: 1,
     loop: true,
@@ -21,3 +22,10 @@ const swiper = new Swiper(".swiper", {
         delay: 5000
     },
 });
+
+// ===  Project Section Active Style  ===
+const tabHover = document.querySelector(".tabs div");
+console.log(tabHover);
+tabHover.addEventListener("click" , () => {
+    tabHover.classList.toggle("tab-hover")
+})
