6b76c7b3e20fe604d35e8bf684f89d99c7c5f252
BT <bahartofigh@gmail.com>
2023-03-13 20:57:47 +0330

    make project tab buttons dynamic and add new text.

--- Js/Index.js
+++ Js/Index.js
@@ -23,12 +23,29 @@ const swiper = new Swiper(".swiper", {
     },
 });
 
+const allImages = [ "./assets/img/project-img3.png" , "./assets/img/project-img2.png" , "./assets/img/project-img1.png" , "./assets/img/project-img3.png" , "./assets/img/project-img2.png" , "./assets/img/project-img1.png"];
+console.log(allImages);
+
 // ===  Project Section Active Style  ===
 const tabHover = document.querySelectorAll(".tab");
+const tabOne = document.querySelector(".tab-one");
+const tabTow = document.querySelector(".tab-two");
+const textTab =  document.querySelector(".tab-two-text");
+const proImages = document.querySelectorAll(".projects-images");
+console.log(proImages);
 
 tabHover.forEach(item => item.addEventListener("click" , activeTab));
 
 function activeTab() {
     tabHover.forEach(item => item.classList.remove("tab-hover"));
-    this.classList.add("tab-hover")
+    this.classList.add("tab-hover");
 }
+
+tabTow.addEventListener("click" , () => {
+    textTab.style.display = "block";
+    proImages.style.display = "none";
+})
+
+tabOne.addEventListener("click" , () => {
+    textTab.style.display = "none";
+})
